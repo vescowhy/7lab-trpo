8077d9c20bf8735492a809ff79b67bd9e92faf49
Gleb Vovk <gleb.vovk.04@mail.ru>
2025-05-31 10:26:52 +0400

    Добавить main.cpp с базовой структурой программы код программы

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Приветственное сообщение
+    cout << "Hello, World!" << endl;
+
+    // Пример изменения
+    int a = 5, b = 3;
+    int sum = a + b;
+
+    // Вывод суммы
+    cout << "Sum of " << a << " and " << b << " is " << sum << endl;
+
+    // Дополнительная логика или изменения могут быть добавлены сюда
+    return 0;
+}
